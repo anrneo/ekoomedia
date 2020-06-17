2e861be3161f4a4974c108e54362174673c2c6d7
anrneo <anreno@gmail.com>
2020-06-17 13:59:08 -0500

    deploy

---
+++ precache-manifest.1c335015e26c09937e81becea72a338e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9b98292df874cd83a9d163b2403778",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dba16c22b459dd3adb5",
+    "url": "/static/css/main.e9bd8e83.chunk.css"
+  },
+  {
+    "revision": "8fa60c33050615ef5191",
+    "url": "/static/js/2.be5bee12.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.be5bee12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dba16c22b459dd3adb5",
+    "url": "/static/js/main.65c015c3.chunk.js"
+  },
+  {
+    "revision": "1ada4112c37185aff733",
+    "url": "/static/js/runtime-main.695ad83c.js"
+  }
+]);
