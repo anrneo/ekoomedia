ea26980ec5de0fd2ed054cbf2b9a65050452f821
anrneo <anreno@gmail.com>
2020-06-17 15:25:40 -0500

    hero

---
+++ precache-manifest.e30d5cbadcf8062c4647f9d75ea0e7c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59be6183acbb249336b00f6a200d9422",
+    "url": "/index.html"
+  },
+  {
+    "revision": "127c8f6e97c856895a00",
+    "url": "/static/css/main.e9bd8e83.chunk.css"
+  },
+  {
+    "revision": "8fa60c33050615ef5191",
+    "url": "/static/js/2.be5bee12.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.be5bee12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "127c8f6e97c856895a00",
+    "url": "/static/js/main.a18f1647.chunk.js"
+  },
+  {
+    "revision": "1ada4112c37185aff733",
+    "url": "/static/js/runtime-main.695ad83c.js"
+  }
+]);
